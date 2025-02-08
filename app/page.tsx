3830f73101a12d97e5e63c4cd18b2129d941f603
mtliendo <mtliendo@focusotter.com>
2025-02-08 15:27:36 -0600

    update wth chatgpt stuff

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
 	return (
 		<Providers>
 			<SuperbowlSquares
-				showScoresAndTeams={false}
+				showScoresAndTeams={true}
 				topTeamName="Kansas City Chiefs 🏈"
 				sideTeamName="Philadelphia Eagles 🦅"
-				topTeamNumbers={[4, 2, 1, 5, 0, 6, 9, 8, 7, 3]}
-				sideTeamNumbers={[8, 0, 1, 2, 3, 9, 4, 6, 7, 5]}
+				topTeamNumbers={[3, 7, 2, 6, 1, 8, 4, 0, 9, 5]}
+				sideTeamNumbers={[5, 0, 9, 4, 8, 7, 6, 1, 2, 3]}
 			/>
 		</Providers>
 	)
