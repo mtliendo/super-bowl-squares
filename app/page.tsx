ae4d7bcdfd37089bf82c615f108bd0ce5833cf93
mtliendo <mtliendo@focusotter.com>
2025-02-09 19:16:02 -0600

    close rest of squares

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 				sideTeamName="Philadelphia Eagles 🦅"
 				topTeamNumbers={[3, 7, 2, 6, 1, 8, 4, 0, 9, 5]}
 				sideTeamNumbers={[5, 0, 9, 4, 8, 7, 6, 1, 2, 3]}
-				disabledQuarters={['FIRST', 'SECOND']}
+				disabledQuarters={['FIRST', 'SECOND', 'THIRD', 'FOURTH']}
 			/>
 		</Providers>
 	)
